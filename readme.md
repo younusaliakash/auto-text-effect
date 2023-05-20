15080a7188b9ca3a60cd5eefa766e662740bde1b
younusaliakash <younusalikash8@gmail.com>
2023-05-20 23:19:44 +0600

    done

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Auto Text Effect
+A very common is an auto typing effect application where an entire sentence is displayed through a typing effect.
+
+<img src="/auto-writing-effect.gif" alt="auto typing effect">
---
+++ script.js
@@ -0,0 +1,21 @@
+const textEl = document.getElementById("text");
+const speedEl = document.getElementById("speed");
+const text = "I am a Javascript Lover";
+let idx = 1
+let speed = 300/speedEl.value;
+
+writeText()
+
+function writeText () {
+    textEl.innerText = text.slice(0, idx)
+
+    idx++
+
+    if(idx > text.length) {
+        idx = 0
+    }
+
+    setTimeout(writeText, speed)
+}
+
+speedEl.addEventListener('input', (e) => speed = 300/e.target.value )
